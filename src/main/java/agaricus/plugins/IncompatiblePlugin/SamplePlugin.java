e37343df09eded3476c6987209e9dcf567244e8a
Agaricus <agaricusb@yahoo.com>
2013-01-26 18:44:34 -0800

    Test getCodeSource() for https://github.com/MinecraftPortCentral/MCPC-Plus/issues/75 java.lang.IllegalArgumentException: URI is not hierarchical

--- src/main/java/agaricus/plugins/IncompatiblePlugin/SamplePlugin.java
+++ src/main/java/agaricus/plugins/IncompatiblePlugin/SamplePlugin.java
@@ -12,6 +12,8 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.*;
+
 /**
  * Sample plugin for Bukkit
  *
@@ -36,6 +38,22 @@ public class SamplePlugin extends JavaPlugin {
     public void onEnable() {
         // TODO: Place any custom enable code here including the registration of any events
 
+        System.out.println("IncompatiblePlugin");
+        
+        try {
+            System.out.println("codeSource URI="+getClass().getProtectionDomain().getCodeSource().getLocation().toURI());
+            System.out.println(" file = ="+new File(getClass().getProtectionDomain().getCodeSource().getLocation().toURI()));
+            System.out.println("new canonical file = ="+(new File(getClass().getProtectionDomain().getCodeSource().getLocation().toURI())).getCanonicalFile());
+        } catch (Throwable t) {
+            System.out.println("codeSource URI exception="+t);
+            t.printStackTrace();
+        }
+
+/*
+        System.out.println("SNOW.id="+net.minecraft.server.v1_4_R1.Block.SNOW.id);
+
+   
+        // test tasks
         Block b = Bukkit.getServer().getWorlds().get(0).getBlockAt(0, 100, 0);
         System.out.println("a="+((CraftChunk)b.getChunk()).getHandle().a(b.getX() & 15, b.getY(), b.getZ() & 15, 48, 0));
 
@@ -46,11 +64,13 @@ public class SamplePlugin extends JavaPlugin {
             }
         });
 
+        // test inheritance remapping
         getLogger().info("creating class inheriting from NMS...");
         IInventory iInventory = new SamplePluginNMSInheritor();
         getLogger().info("iInventory= "+iInventory);
         // if subclass/implementator not remapped: java.lang.AbstractMethodError: SamplePluginNMSInheritor.k_()I
         getLogger().info("getSize="+iInventory.getSize());
+        */
 
         // Register our events
         PluginManager pm = getServer().getPluginManager();
